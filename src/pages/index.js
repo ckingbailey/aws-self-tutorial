099d60f54d87ddb1a4fb98a563861acec9c2530d
Colin <ckingbailey@gmail.com>
2018-09-27 17:09:33 -0700

    add Hello World and personal intro to page 1 (#3)

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,12 @@ import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Hello World!</h1>
+    <p>My name's Colin</p>
+    <p>I write HTML, CSS, JS, PHP, and MySQL</p>
+    <p>I've also mucked around with a little Ruby, Python, and Java</p>
+    <p>I just finished a contract building a data management app related to the BART extension to San Jose</p>
+    <p>I'm excited to build something 🤩<span className="hightlight-text">wow</span>🤩 with a bunch of great people at JAMstack Conf 2018</p>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
